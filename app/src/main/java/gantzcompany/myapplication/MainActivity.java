f7edfd8628b36914d4fe33ea3378b23387ceff1b
Gantz7774 <jgabrielhf100@gmail.com>
2020-09-08 23:52:14 -0500

    cambios 08/09/2020 interfaz del listado

--- app/src/main/java/gantzcompany/myapplication/MainActivity.java
+++ app/src/main/java/gantzcompany/myapplication/MainActivity.java
@@ -99,9 +99,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        if(nAuth.getCurrentUser() != null){
 
-
-        }
     }
 }
